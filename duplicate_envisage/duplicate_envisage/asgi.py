b2d004a2b43d755c05ee646df6899cfbbcacccad
Shahid1725 <shahid.v@twistedmountainanimation.com>
2026-05-29 19:27:53 +0530

    message

---
+++ duplicate_envisage/duplicate_envisage/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for duplicate_envisage project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'duplicate_envisage.settings')
+
+application = get_asgi_application()
